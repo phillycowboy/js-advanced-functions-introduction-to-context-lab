7d38a110a5f3ab810a5ce78ef2363d1b75fedf7d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-05-02 15:52:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {
+ const employee = {
+     firstName: `${firstName}`,
+     familyName: `${familyName}`,
+     title: `${title}`,
+     payPerHour: `${payPerHour}`,
+     timeInEvents: [],
+     timeOutEvents: []
+    
+}
+ return employee;
+}
+
+function createEmployeeRecords() {
+    
+}
+
+// coming back to this later this makes no sense today
